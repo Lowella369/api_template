69a60fa153bda4e1217925d36cc0e27665447482
Fox, Lowella <lowella.fox@verizon.com>
2022-04-06 18:51:37 -0500

    Adding authentication and authorization to the user

--- app.js
+++ app.js
@@ -15,52 +15,112 @@ const app = express();
 // specify out request bodies are json
 app.use(express.json());
 
+// configure basicAuth
+app.use(basicAuth({
+  authorizer : dbAuthorizer,
+  authorizeAsync : true,
+  unauthorizedResponse : () => "You do not have access to this content"
+}))
+
+// compare username and password with db content
+// return boolean indicating password match
+async function dbAuthorizer(username, password, callback){
+  try {
+    // get matching user from db
+    const user = await User.findOne({where: {name:username}})
+    // if username is valid compare passwords
+    let isValid = (user != null ) ? await bcrypt.compare(password, user.password) : false;
+    callback(null, isValid)
+  } catch(err) {
+    //if authorize fails, log error
+    console.log("Error: ", err)
+    callback(null, false)
+  }
+}
+
 // routes go here
 app.get('/', (req, res) => {
   res.send('<h1>App Running</h1>')
 })
 
-app.get('/items', async(req, res) => {
-  let items = await Item.findAll();
-  res.json({items});
+// read all users
+app.get('/users', async (req,res) =>{
+  let users = await User.findAll()
+  res.json({users})
 })
 
-app.post('/items', async(req,res) =>{
-  let newItem = await Item.create(req.body);
-  res.json({newItem})
+// read one user by id
+app.get('/users/:id', async (req, res) => {
+  let user = await User.findByPk(req.params.id);
+  res.json({user});
 })
 
-app.get('/users', async(req, res) => {
-  let users = await User.findAll();
-  res.json({users});
-})
+// create user
+app.post('/users', async (req,res) => {
+  const name = req.body.name;
+  const password = req.body.password;
 
-app.post('/users', async(req,res) =>{
-
-  bcrypt.hash(req.body.password,saltRounds, async function(err,hash){
-    let newUser = await User.create({'name':req.body.name, 'password':hash});
-    console.log(hash)
+  bcrypt.hash(password, saltRounds, async function (err,hash){
+    const newUser = await User.create({'name': name, 'password': hash})
     res.json({newUser})
   })
-
 })
 
-app.post('/session', async(req,res) =>{
+//create new session
+app.post('/session', async (req,res) => {
   const thisUser = await User.findOne({
-    where: {name:req.body.name}
+    where: {name: req.body.name}
   })
   if(!thisUser){
-    res.send('User not found')
+    res.send("User not found")
   } else {
-    bcrypt.compare(req.body.password, thisUser.password, async function (err, result){
+    bcrypt.compare(req.body.password, thisUser.password, async function (err,result) {
       if(result){
+        console.log(thisUser.password)
         res.json(thisUser)
       } else {
         res.send("Passwords do not match")
       }
     })
   }
+
 })
+
+
+
+// read all items
+app.get('/items', async(req, res)=> {
+  let items = await Item.findAll();
+  res.json({items});
+})
+
+// read item by id
+app.get('/items/:id', async(req, res)=> {
+  let item = await Item.findByPk(req.params.id);
+  res.json({item});
+})
+
+// delete item
+app.delete('/items/:id', async(req, res)=> {
+  await Item.destroy({where: {id: req.params.id}});
+  res.send('Deleted!')
+})
+
+// create item
+app.post('/items', async(req, res)=> {
+  let newItem = await Item.create(req.body);
+  res.json({newItem})
+})
+
+// update item
+app.put('/items/:id', async(req, res)=> {
+  let updatedItem = await Item.update(req.body, {
+    where : {id : req.params.id}
+  });
+  res.json({updatedItem})
+})
+
+
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
